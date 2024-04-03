606b16f132181ad985555d19f752f824eee5739a
fannyjson <147634432+fannyjson@users.noreply.github.com>
2024-04-03 11:39:47 +0200

    reads_comparison_18S_metazoa.R

--- read_comparison_18S_metazoa.R
+++ read_comparison_18S_metazoa.R
@@ -22,6 +22,7 @@ seqtab_18S_all=read.table("C:/Users/johan/OneDrive/R/Master project/seqtab_18S_a
 #Load libraries 
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 library(vegan)
 
 #################################################################################
@@ -46,6 +47,10 @@ replace <- function(vector) {
 
 
 taxa_18S[] <- lapply(taxa_18S, replace)
+
+#Number of reads for annotated 18S reads
+all_reads=sum(seqtab_18S_all)
+metazoa_reads=sum(seqtab_18S)
 
 ########################## #18S ALL ############################################
 
@@ -73,6 +78,32 @@ for (i in 1:ncol(norm_seqtab_18S_all)) {
 
 
 #Summarising counts at different taxonomic levels
+#Normalized clade count
+
+clade_counts_norm=list()
+
+for (i in 1:ncol(taxa_18S)){
+  matr_norm=NULL
+  clade=unique(taxa_18S[,i])
+  clade=clade[!is.na(clade)]
+  for (j in 1:length(clade)){
+    ix=which(clade[j]==taxa_18S[,i])
+    if (length(ix)>1){
+      matr_norm=rbind(matr_norm, apply(norm_seqtab_18S_all[ix,],2,sum,na.rm=TRUE))
+    } else {
+      matr_norm=rbind(matr_norm, norm_seqtab_18S_all[ix,])
+    }
+  }
+  rownames(matr_norm)=clade
+  
+  clade_counts_norm[[i]]=matr_norm
+}
+
+matr_norm[is.na(matr_norm)]=0
+matr_norm=matr_norm[rowSums(matr_norm !=0, na.rm=TRUE)>0 , , drop=FALSE]
+
+#Non-normalized clade count
+
 clade_counts=list()
 
 for (i in 1:ncol(taxa_18S)){
@@ -82,9 +113,9 @@ for (i in 1:ncol(taxa_18S)){
   for (j in 1:length(clade)){
     ix=which(clade[j]==taxa_18S[,i])
     if (length(ix)>1){
-      matr=rbind(matr, apply(norm_seqtab_18S_all[ix,],2,sum,na.rm=TRUE))
+      matr=rbind(matr, apply(seqtab_18S_all[ix,],2,sum,na.rm=TRUE))
     } else {
-      matr=rbind(matr, norm_seqtab_18S_all[ix,])
+      matr=rbind(matr, seqtab_18S_all[ix,])
     }
   }
   rownames(matr)=clade
@@ -93,75 +124,69 @@ for (i in 1:ncol(taxa_18S)){
   clade_counts[[i]]=matr
 }
 
+matr[is.na(matr)]=0
+matr=matr[rowSums(matr !=0, na.rm=TRUE)>0 , , drop=FALSE]
+
 
 # Total reads for each taxa (18S)
 
-domain_reads = clade_counts[[1]]
-domain_reads = data.frame(domain_reads)
-domain_reads = na.omit(domain_reads)
-sum_domain_reads=sum(domain_reads)
+domain_reads = na.omit(clade_counts[[1]])
+domain_reads = sum(domain_reads)
 
-supergroup_reads = clade_counts[[2]]
-supergroup_reads = data.frame(supergroup_reads)
-supergroup_reads = na.omit(supergroup_reads)
-sum_supergroup_reads = sum(supergroup_reads)
+supergroup_reads = na.omit(clade_counts[[2]])
+supergroup_reads = sum(supergroup_reads)
 
-division_reads = clade_counts[[3]]
-division_reads = data.frame(division_reads)
-division_reads = na.omit(division_reads)
-sum_division_reads = sum(division_reads)
+division_reads = na.omit(clade_counts[[3]])
+division_reads = sum(division_reads)
 
-subdivision_reads = clade_counts[[4]]
-subdivision_reads = data.frame(subdivision_reads)
-subdivision_reads = na.omit(subdivision_reads)
-sum_subdivision_reads = sum(subdivision_reads)
+subdivision_reads = na.omit(clade_counts[[4]])
+subdivision_reads = sum(division_reads)
 
-class_reads = clade_counts[[5]]
-class_reads = data.frame(class_reads)
-class_reads = na.omit(class_reads)
-sum_class_reads = sum(class_reads)
+class_reads = na.omit(clade_counts[[5]])
+class_reads = sum(class_reads)
 
-order_reads = clade_counts[[6]]
-order_reads = data.frame(order_reads)
-order_reads = na.omit(order_reads)
-sum_order_reads = sum(order_reads)
+order_reads = na.omit(clade_counts[[6]])
+order_reads = sum(order_reads)
 
-family_reads = clade_counts[[7]]
-family_reads = data.frame(family_reads)
-family_reads = na.omit(family_reads)
-sum_family_reads = sum(family_reads)
+family_reads = na.omit(clade_counts[[7]])
+family_reads= sum(family_reads)
 
-genus_reads = clade_counts[[8]]
-genus_reads = data.frame(genus_reads)
-genus_reads = na.omit(genus_reads)
-sum_genus_reads = sum(genus_reads)
+genus_reads = na.omit(clade_counts[[8]])
+genus_reads = sum(genus_reads)
 
-species_reads = clade_counts[[9]]
-species_reads = data.frame(species_reads)
-species_reads = na.omit(species_reads)
-species_reads_sum = sum(species_reads)
-
-all_reads=sum_domain_reads+sum_supergroup_reads+sum_division_reads+sum_subdivision_reads+sum_class_reads+sum_order_reads+sum_family_reads+sum_genus_reads+sum_family_reads
-
-taxonomic_levels = list(domain_reads, supergroup_reads, division_reads, subdivision_reads, class_reads, order_reads, family_reads, genus_reads, species_reads)
-
-sums <- lapply(taxonomic_levels, function(df) sum(unlist(df), na.rm = TRUE))
+species_reads = na.omit(clade_counts[[9]])
+species_reads = sum(species_reads)
 
 
-all_reads_sum = colSums(do.call(cbind, sums), na.rm = TRUE)
+# Create a list to store taxonomic levels and sums
+taxonomic_levels <- list()
+taxonomic_sums <- list()
+
+# Loop through each clade_counts element
+for (i in 1:9) {
+  # Extract clade counts
+  clade <- clade_counts[[i]]
+  
+  # Convert to data frame and remove NAs
+  clade_df <- na.omit(data.frame(clade))
+  
+  # Calculate sum for each taxonomic level
+  taxonomic_sums[[i]] <- sum(unlist(clade_df), na.rm = TRUE)
+  
+  # Store taxonomic levels
+  taxonomic_levels[[i]] <- clade_df
+}
+
+# Calculate sum of all reads
+sum_domain_reads <- sum(unlist(taxonomic_sums), na.rm = TRUE)
 
 # Calculate fractions
-fractions = sapply(sums, function(category_sums) category_sums / sum_domain_reads * 100)
-fractions_df=data.frame(fractions)
-rownames(fractions_df)=c("Domain", "Supergroup", "Division", "Subdivision", "Class", "Order", "Family", "Genus", "Species")
-print(fractions)
+fractions <- sapply(taxonomic_sums, function(category_sums) category_sums / taxonomic_sums[[1]] * 100)
+# Create a data frame with row names
+fractions_df <- data.frame(fractions)
+rownames(fractions_df) <- c("Domain", "Supergroup", "Division", "Subdivision", "Class", "Order", "Family", "Genus", "Species")
 
-# Print nr of unique taxa per level
-                   
-taxlvl_18S = taxa_18S%>%
-  summarise(across(everything(), ~length(unique(na.omit(.)))))
-
-print(taxlvl_18S)
+print(fractions_df)
                    
 #################### 18S (METAZOA ONLY) #######################################
 
@@ -189,6 +214,34 @@ for (i in 1:ncol(seqtab_18S)) {
 }
 
 #Summarising counts at different taxonomic levels
+#Normalized
+
+norm_meta_clade_counts=list()
+
+for (i in 1:ncol(taxa_metazoa)){
+  norm_meta_matr=NULL
+  clade=unique(taxa_metazoa[,i])
+  clade=clade[!is.na(clade)]
+  for (j in 1:length(clade)){
+    ix=which(clade[j]==taxa_metazoa[,i])
+    if (length(ix)>1){
+      norm_meta_matr=rbind(norm_meta_matr, apply(norm_seqtab_18S[ix,],2,sum,na.rm=TRUE))
+    } else {
+      norm_meta_matr=rbind(norm_meta_matr, norm_seqtab_18S[ix,])
+    }
+  }
+  
+  rownames(norm_meta_matr)=clade
+  #colnames(matr)=colnames(norm_matr)=sample_id
+  
+  norm_meta_clade_counts[[i]]=norm_meta_matr
+}
+
+norm_meta_matr[is.na(norm_meta_matr)]=0
+norm_meta_matr=norm_meta_matr[rowSums(norm_meta_matr !=0, na.rm=TRUE)>0 , , drop=FALSE]
+
+#Non-normalized
+
 meta_clade_counts=list()
 
 for (i in 1:ncol(taxa_metazoa)){
@@ -198,98 +251,91 @@ for (i in 1:ncol(taxa_metazoa)){
   for (j in 1:length(clade)){
     ix=which(clade[j]==taxa_metazoa[,i])
     if (length(ix)>1){
-      meta_matr=rbind(meta_matr, apply(norm_seqtab_18S[ix,],2,sum,na.rm=TRUE))
+      meta_matr=rbind(meta_matr, apply(seqtab_18S[ix,],2,sum,na.rm=TRUE))
     } else {
-      meta_matr=rbind(meta_matr, norm_seqtab_18S[ix,])
+      meta_matr=rbind(meta_matr, seqtab_18S[ix,])
     }
   }
   
   rownames(meta_matr)=clade
-  #colnames(matr)=colnames(norm_matr)=sample_id
   
   meta_clade_counts[[i]]=meta_matr
 }
 
 
+meta_matr[is.na(meta_matr)]=0
+meta_matr=matr[rowSums(meta_matr !=0, na.rm=TRUE)>0 , , drop=FALSE]
+
 # Total reads for each taxa (18S)
 
-domain_reads = meta_clade_counts[[1]]
-domain_reads = data.frame(domain_reads)
-domain_reads = na.omit(domain_reads)
-sum_domain_reads=sum(domain_reads)
+sum(seqtab_18S)
 
-supergroup_reads = meta_clade_counts[[2]]
-supergroup_reads = data.frame(supergroup_reads)
-supergroup_reads = na.omit(supergroup_reads)
-sum_supergroup_reads = sum(supergroup_reads)
+domain_reads_meta = na.omit(meta_clade_counts[[1]])
+domain_reads_meta = sum(domain_reads_meta)
 
-division_reads = meta_clade_counts[[3]]
-division_reads = data.frame(division_reads)
-division_reads = na.omit(division_reads)
-sum_division_reads = sum(division_reads)
+supergroup_reads_meta = na.omit(meta_clade_counts[[2]])
+supergroup_reads_meta = sum(supergroup_reads_meta)
 
-subdivision_reads = meta_clade_counts[[4]]
-subdivision_reads = data.frame(subdivision_reads)
-subdivision_reads = na.omit(subdivision_reads)
-sum_subdivision_reads = sum(subdivision_reads)
+division_reads_meta = na.omit(meta_clade_counts[[3]])
+division_reads_meta = sum(division_reads_meta)
 
-class_reads = meta_clade_counts[[5]]
-class_reads = data.frame(class_reads)
-class_reads = na.omit(class_reads)
-sum_class_reads = sum(class_reads)
+subdivision_reads_meta = na.omit(meta_clade_counts[[4]])
+subdivision_reads_meta = sum(division_reads_meta)
 
-order_reads = meta_clade_counts[[6]]
-order_reads = data.frame(order_reads)
-order_reads = na.omit(order_reads)
-sum_order_reads = sum(order_reads)
+class_reads_meta = na.omit(meta_clade_counts[[5]])
+class_reads_meta = sum(class_reads_meta)
 
-family_reads = meta_clade_counts[[7]]
-family_reads = data.frame(family_reads)
-family_reads = na.omit(family_reads)
-sum_family_reads = sum(family_reads)
+order_reads_meta = na.omit(meta_clade_counts[[6]])
+order_reads_meta = sum(order_reads_meta)
 
-genus_reads = meta_clade_counts[[8]]
-genus_reads = data.frame(genus_reads)
-genus_reads = na.omit(genus_reads)
-sum_genus_reads = sum(genus_reads)
+family_reads_meta = na.omit(meta_clade_counts[[7]])
+family_reads_meta= sum(family_reads_meta)
 
-species_reads = meta_clade_counts[[9]]
-species_reads = data.frame(species_reads)
-species_reads = na.omit(species_reads)
-species_reads_sum = sum(species_reads)
+genus_reads_meta = na.omit(meta_clade_counts[[8]])
+genus_reads_meta = sum(genus_reads_meta)
 
-all_reads=sum_domain_reads+sum_supergroup_reads+sum_division_reads+sum_subdivision_reads+sum_class_reads+sum_order_reads+sum_family_reads+sum_genus_reads+sum_family_reads
+species_reads_meta = na.omit(meta_clade_counts[[9]])
+species_reads_meta = sum(species_reads_meta)
 
 
-taxonomic_levels = list(domain_reads, supergroup_reads, division_reads, subdivision_reads, class_reads, order_reads, family_reads, genus_reads, species_reads)
+# Create a list to store taxonomic levels and sums
+taxonomic_levels_meta <- list()
+taxonomic_sums_meta <- list()
 
-sums <- lapply(taxonomic_levels, function(df) sum(unlist(df), na.rm = TRUE))
+# Loop through each clade_counts element
+for (i in 1:9) {
+  # Extract clade counts
+  meta_clade <- meta_clade_counts[[i]]
+  
+  # Convert to data frame and remove NAs
+  meta_clade_df <- na.omit(data.frame(meta_clade))
+  
+  # Calculate sum for each taxonomic level
+  taxonomic_sums_meta[[i]] <- sum(unlist(meta_clade_df), na.rm = TRUE)
+  
+  # Store taxonomic levels
+  taxonomic_levels_meta[[i]] <- meta_clade_df
+}
 
 
-all_reads_sum = colSums(do.call(cbind, sums), na.rm = TRUE)
+# Calculate sum of all reads
+sum_domain_reads_meta <- sum(unlist(taxonomic_sums_meta), na.rm = TRUE)
 
 # Calculate fractions
-fractions_met= sapply(sums, function(category_sums) category_sums / sum_domain_reads * 100)
-fractions_df_met=data.frame(fractions_met)
-rownames(fractions_df_met)=c("Domain", "Supergroup", "Division", "Subdivision", "Class", "Order", "Family", "Genus", "Species")
-print(fractions_met)
+fractions_meta <- sapply(taxonomic_sums_meta, function(category_sums_meta) category_sums_meta/ taxonomic_sums_meta[[1]] * 100)
+# Create a data frame with row names
+fractions_df_meta <- data.frame(fractions_meta)
+rownames(fractions_df_meta) <- c("Domain", "Supergroup", "Division", "Subdivision", "Class", "Order", "Family", "Genus", "Species")
 
-# Print nr of unique taxa per level
-taxlvl_metazoa = taxa_metazoa%>%
-  summarise(across(everything(), ~length(unique(na.omit(.)))))
-
-print(taxlvl_metazoa)
+print(fractions_df_meta)
 
 ################## PLOT ########################################################
 
 # Merge
-merged_fractions = merge(fractions_df, fractions_df_met, by = "row.names", all = TRUE)
+merged_fractions = merge(fractions_df, fractions_df_meta, by = "row.names", all = TRUE)
 colnames(merged_fractions)[colnames(merged_fractions)=="Row.names"]="Taxa"
 colnames(merged_fractions)[colnames(merged_fractions)=="fractions"]="All 18S"
 colnames(merged_fractions)[colnames(merged_fractions)=="fractions_met"]="Metazoa"
-
-library(ggplot2)
-library(tidyr)
 
 long_data = gather(merged_fractions, key = "Category", value = "Value", -Taxa)
 taxa_order = c("Domain", "Supergroup", "Division", "Subdivision", "Class", "Order", "Family", "Genus", "Species")
@@ -312,8 +358,6 @@ p<-ggplot(long_data, aes(x = Taxa, y = Value, fill = Category)) +
 print(p)
 ggsave <- function(..., bg = 'white') ggplot2::ggsave(..., bg = bg)
 ggsave("C:/Users/johan/OneDrive/R/Master project/plots/reads_distribution.png",p)
-
-
 
 ############## NMDS & Shannon ###########################################################
 
@@ -338,8 +382,9 @@ nmds_data <- cbind(nmds_coordinates, Shannon_Diversity = shannon_values)
 library(ggplot2)
 final_nmds <- ggplot(nmds_data, aes(x = MDS1, y = MDS2, color = Shannon_Diversity)) +
   geom_point() +
-  scale_color_gradient(name = "Shannon Diversity") +
-  ggtitle("NMDS plot showing shannon diversity for ASVs in each sample")
+  scale_color_gradient(name = "Shannon Diversity", low = "black", high = "white") +
+  ggtitle("NMDS plot showing shannon diversity for ASVs in each sample")+ 
+  theme_bw()
 
 # Print the plot
 print(final_nmds)
